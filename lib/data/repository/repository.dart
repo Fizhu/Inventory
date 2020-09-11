df2343e68033da89bf8a20acc0e3ea0368f04e50
Hafizh Anbiya <hvyz.anbiya@gmail.com>
2020-09-11 18:02:53 +0700

    adding repository

---
+++ lib/data/repository/repository.dart
@@ -0,0 +1,7 @@
+import 'package:inventory/data/models/data.dart';
+
+abstract class Repository {
+  Future<ResponseData> login(String email, String password);
+
+  Future<ResponseList> getBarangById(int idUser);
+}
