3adc9f858bbd2bbbdb46f0134dde2d4d087a0873
Hafizh Anbiya <hvyz.anbiya@gmail.com>
2020-09-21 23:49:19 +0700

    update date time picker in add page

--- lib/ui/add/add.dart
+++ lib/ui/add/add.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
+import 'package:inventory/utils/app_constant.dart';
 
 class AddPage extends StatefulWidget {
   static const routeName = '/add';
@@ -8,6 +10,17 @@ class AddPage extends StatefulWidget {
 }
 
 class _AddPageState extends State<AddPage> {
+  DateTime _dateTimeNow = DateTime.now();
+  TextEditingController _dateController = TextEditingController();
+  DateFormat _formatDateTime = DateFormat(AppConstant.DATE_TIME_FORMAT);
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    _dateController.text = _formatDateTime.format(_dateTimeNow);
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -20,21 +33,62 @@ class _AddPageState extends State<AddPage> {
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.stretch,
               children: [
-                _formInput(
-                    TextInputType.text, 'Item Name', null, (value) {}, (value) {}),
-                SizedBox(height: 16.0,),
-                _formInput(
-                    TextInputType.text, 'Description', null, (value) {}, (value) {}),
-                SizedBox(height: 16.0,),
-                _formInput(
-                    TextInputType.text, 'Date', IconButton(
-                  icon: Icon(Icons.date_range),
-                  onPressed: () {},
-                ), (value) {}, (value) {}),
+                _formInput(TextInputType.text, 'Item Name', null, (value) {},
+                    (value) {}),
+                SizedBox(
+                  height: 16.0,
+                ),
+                _formInput(TextInputType.text, 'Description', null, (value) {},
+                    (value) {}),
+                SizedBox(
+                  height: 16.0,
+                ),
+                Container(
+                  child: TextFormField(
+                    keyboardType: TextInputType.datetime,
+                    onSaved: (String value) {},
+                    validator: (String value) {},
+                    controller: _dateController,
+                    readOnly: true,
+                    decoration: InputDecoration(
+                      labelText: 'Date Time',
+                      suffixIcon: IconButton(
+                        icon: Icon(Icons.date_range),
+                        onPressed: () {
+                          _selectDate(context);
+                        },
+                      ),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
         ));
+  }
+
+  _selectDate(BuildContext context) async {
+    final DateTime dateTime = await showDatePicker(
+        context: context,
+        initialDate: _dateTimeNow,
+        firstDate: DateTime(2000),
+        lastDate: DateTime(2025),
+        helpText: 'Select Date',
+        cancelText: 'Cancel',
+        confirmText: 'Select');
+    if (dateTime != null && dateTime != _dateTimeNow) {
+      var selectedDateTime = DateTime(
+          dateTime.year,
+          dateTime.month,
+          dateTime.day,
+          DateTime.now().hour,
+          DateTime.now().minute,
+          DateTime.now().second);
+      _dateController.text = _formatDateTime.format(selectedDateTime);
+    }
   }
 
   _formInput(TextInputType textInputType, String label, Widget suffixIcon,
--- lib/utils/app_constant.dart
+++ lib/utils/app_constant.dart
@@ -1,4 +1,5 @@
 class AppConstant {
   static const BASE_URL = 'https://bus.unila.ac.id/crud/api/v1/';
   static const AUTHOR = 'Fizhu';
+  static const DATE_TIME_FORMAT = 'yyyy-MM-dd HH:mm:ss';
 }
